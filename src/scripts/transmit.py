68742d38a235b8d9eaa6cc362efc1b0644869331
Alex Mark <alexthemark@gmail.com>
2022-09-08 17:35:52 -0400

    Try more

--- src/scripts/transmit.py
+++ src/scripts/transmit.py
@@ -1,5 +1,17 @@
 import os
+import base64
+import requests
 
-print("hello world")
 api_key = os.getenv("MANIFEST_API_KEY")
 bom_filepath = os.getenv("BOM_FILEPATH")
+
+bom = open(bom_filepath, "r")
+bom_string = bom.read()
+base64BomContents = base64.b64encode(bom_string)
+bom.close()
+
+r = requests.put("https://mvdryhw7l8.execute-api.us-east-1.amazonaws.com/prod/receive",
+                 data={"base64BomContents": base64BomContents, "apiKey": api_key}, headers={"Content-Type": "application/json"})
+
+print(r.text)
+print(r.status_code)
