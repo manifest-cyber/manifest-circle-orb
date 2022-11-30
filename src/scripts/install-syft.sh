98507e541415351638d204e283d9877f88ab89b6
manifestori <ori@manifestcyber.com>
2022-11-30 23:05:26 +0200

    fix: ci

--- src/scripts/install-syft.sh
+++ src/scripts/install-syft.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | bash -s -- -b /usr/local/bin \
-        export PATH="/usr/local/bin/:$PATH"
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ./bin
+
+export PATH="./bin/:$PATH"
