b66109d719a1f0fc549113d1b37c1e1906970d71
manifestori <ori@manifestcyber.com>
2022-12-09 20:54:09 +0200

    feat: support name field

--- src/scripts/transmit.py
+++ src/scripts/transmit.py
@@ -5,15 +5,15 @@ import json
 import urllib.request # It would be hard to make circle import requests
 
 api_key = os.environ.get('MANIFEST_API_KEY')
-bom_filepath = os.getenv("BOM_FILEPATH")
+filepath = os.getenv("SBOM_FILEPATH")
 
 
-bom = open(bom_filepath, "r")
-bom_string = bom.read()
-base64BomContents = base64.b64encode(bom_string.encode("utf-8")).decode("utf-8")
-bom.close()
+sbom = open(filepath, "r")
+sbom_string = sbom.read()
+base64BomContents = base64.b64encode(sbom_string.encode("utf-8")).decode("utf-8")
+sbom.close()
 
-data = json.dumps({"base64BomContents": base64BomContents, "source": "circle-ci", "filename": bom_filepath, "relationship": "first"})
+data = json.dumps({"base64BomContents": base64BomContents, "source": "circle-ci", "filename": filepath, "relationship": "first"})
 url = "https://api.manifestcyber.com/v1/sbom/upload"
 headers = {"Content-Type": "application/json", "Authorization": "Bearer " + api_key}
 
